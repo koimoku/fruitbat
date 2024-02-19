388449065b9582ee9d1346bf54d2b4e180dc4a34
koimoku <79475755+koimoku@users.noreply.github.com>
2024-02-18 23:19:48 -0600

    Delete public_html directory

--- public_html/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("collapseMenuButton").addEventListener("click", function() {
-    let leftSideBar = document.getElementById("leftSideBar");
-    let postButtonContainer = document.getElementById("postButtonContainer");
-
-    leftSideBar.classList.toggle("active");
-    postButtonContainer.classList.toggle("active");
-});
